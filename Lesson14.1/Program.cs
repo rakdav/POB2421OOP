39e86094eb153b484b134d92c0c51c69b61d2e69
rakdav <rakdav@inbox.ru>
2024-10-21 21:25:47 +0200

    Lesson14Last

--- Lesson14.1/Program.cs
+++ Lesson14.1/Program.cs
@@ -1,9 +1,10 @@
-﻿List<Car> GetCars()
+﻿
+List<Car> GetCars()
 {
     return new List<Car>
                 {
                     new Car { VIN = "ABC123", Make = "Ford",
-                            Model = "F-250", Year = 2000 },
+                            Model = "F-250", Year = 2010 },
                     new Car { VIN = "DEF123", Make = "BMW",
                             Model = "Z-3", Year = 2005 },
                     new Car { VIN = "ABC456", Make = "Audi",
@@ -11,9 +12,50 @@
                     new Car { VIN = "HIJ123", Make = "VW",
                             Model = "Bug",  Year = 1956  },
                     new Car { VIN = "DEF456", Make = "Ford",
-                            Model = "F-150", Year = 1998 }
+                            Model = "F-150", Year = 2005 }
                 };
 }
+//традиционный способ
+//bool FilterYear(Car car) { return car.Year > 2000; }
+//bool FilterModel(Car car) { return car.Model.StartsWith("F"); }
+
+//List<Car> cars = GetCars();
+//List<Car> filtered = new List<Car>();
+//foreach (Car car in cars)
+//{
+//    if (FilterYear(car) && FilterModel(car))
+//    {
+//        filtered.Add(car);
+//    }
+//}
+//foreach(var i in filtered) 
+//    Console.WriteLine(i.Model+" "+i.Year);
+//Console.WriteLine();
+//2
+List<FilterPredicate> predicates = new List<FilterPredicate>();
+//predicates.Add(delegate (Car car) { return car.Year > 2000; });
+//predicates.Add(delegate (Car car) { return car.Model.StartsWith("F");});
+//predicates.Add((Car car)=>car.Year > 2000);
+//predicates.Add((Car car)=>car.Model.StartsWith("F"));
+predicates.Add(car => car.Year > 2000);
+predicates.Add(car => car.Model.StartsWith("F"));
+bool CheckPredicates(Car car, IList<FilterPredicate> predicates)
+{
+    foreach (FilterPredicate p in predicates)
+    {
+        if (!p(car)) { return false; }
+    }
+    return true;
+}
+List<Car> cars = GetCars();
+List<Car> filtered = new List<Car>();
+foreach (Car car in cars)
+{
+    if (CheckPredicates(car, predicates)) { filtered.Add(car); }
+}
+foreach (var i in filtered)
+    Console.WriteLine(i.Model + " " + i.Year);
+
 
 public class Car
 {
@@ -23,4 +65,6 @@ public class Car
     public int Year { get; set; }
     public string Color { get; set; }
 }
+public delegate bool FilterPredicate(Car car);
+
 
---
+++ Lesson14.2/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] Mult(int[] mas, Predicate<int> func)
+{
+    List<int> result = new List<int>();
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (func(mas[i])) result.Add(mas[i]);
+    }
+    return result.ToArray();
+}
+DateTime[] FilterDate(DateTime[] mas, Predicate<DateTime> func)
+{
+    List<DateTime> result = new List<DateTime>();
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (func(mas[i])) result.Add(mas[i]);
+    }
+    return result.ToArray();
+}
+int[] mas = new int[10];
+Random random = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(10, 100);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine();
+foreach (int i in Mult(mas, x => x % 2 == 0))
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+DateTime[] mdate = new DateTime[3]
+{
+    new DateTime(2024,12,5),
+    new DateTime(2024,10,5),
+    new DateTime(2024,7,9)
+};
+foreach (DateTime i in FilterDate(mdate, x => x>DateTime.Now))
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
