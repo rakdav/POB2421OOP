a32ead05b6c1fabc7ade423fce937e8ab887b7d3
rakdav <rakdav@inbox.ru>
2024-10-21 20:54:32 +0200

    l1

--- Lesson14.1/Program.cs
+++ Lesson14.1/Program.cs
@@ -1,4 +1,21 @@
-﻿public class Car
+﻿List<Car> GetCars()
+{
+    return new List<Car>
+                {
+                    new Car { VIN = "ABC123", Make = "Ford",
+                            Model = "F-250", Year = 2000 },
+                    new Car { VIN = "DEF123", Make = "BMW",
+                            Model = "Z-3", Year = 2005 },
+                    new Car { VIN = "ABC456", Make = "Audi",
+                            Model = "TT", Year = 2008 },
+                    new Car { VIN = "HIJ123", Make = "VW",
+                            Model = "Bug",  Year = 1956  },
+                    new Car { VIN = "DEF456", Make = "Ford",
+                            Model = "F-150", Year = 1998 }
+                };
+}
+
+public class Car
 {
     public string VIN { get; set; }
     public string Make { get; set; }
