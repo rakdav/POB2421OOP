dbf89f75f30916e0e59950e78ca6b035bc5333bd
rakdav <rakdav@inbox.ru>
2024-10-23 21:23:34 +0200

    Lesson15

---
+++ Lesson15.1/Program.cs
@@ -0,0 +1,119 @@
+﻿KeyEvent keyEvent = new KeyEvent();
+keyEvent.KeyDown += (sender, e) =>
+{
+    switch (e.ch) 
+    {
+        case 'C':
+            {
+                MyColor(true);
+                break;
+            }
+        case 'B':
+            {
+                MyColor(false);
+                break;
+            }
+        case 'S':
+            {
+
+            }
+            break;
+    }
+};
+ConsoleTitle();
+char ch=' ';
+do
+{
+    Console.Write("Введите команду:");
+    ConsoleKeyInfo key = Console.ReadKey();
+    ch = key.KeyChar;
+}
+while (ch!='E');
+void CC(ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+}
+void Command(string s1,string s2)
+{
+    CC(ConsoleColor.Red);
+    Console.Write(s1);
+    CC(ConsoleColor.White);
+    Console.Write(" - "+s2+"\n");
+}
+void ConsoleTitle()
+{
+    CC(ConsoleColor.Green);
+    Console.WriteLine("*********************************\n" +
+        "Программа настройки консоли\n" +
+        "*********************************");
+    CC(ConsoleColor.Yellow);
+    Console.WriteLine("Управляющие команды:");
+    Command("C", "Поменять цвет текста");
+    Command("B", "Поменять цвет фона");
+    Command("S", "Поменять размер окна");
+    Command("T", "Поменять заголовок");
+    Command("R", "Сбросить изменения");
+    Command("E", "Выход");
+    Console.WriteLine();
+}
+void MyColor(bool F_or_B)
+{
+    Console.WriteLine("Введите цвет:");
+    string s=Console.ReadLine()!;
+    switch(s)
+    {
+        case "Black":
+            {
+                if(F_or_B) Console.ForegroundColor= ConsoleColor.Black;
+                else Console.BackgroundColor= ConsoleColor.Black;
+            }
+            break;
+        case "Yellow":
+            {
+                if (F_or_B) Console.ForegroundColor = ConsoleColor.Yellow;
+                else Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+            break;
+        case "Green":
+            {
+                if (F_or_B) Console.ForegroundColor = ConsoleColor.Green;
+                else Console.BackgroundColor = ConsoleColor.Green;
+            }
+            break;
+        case "Red":
+            {
+                if (F_or_B) Console.ForegroundColor = ConsoleColor.Red;
+                else Console.BackgroundColor = ConsoleColor.Red;
+            }
+            break;
+        case "Blue":
+            {
+                if (F_or_B) Console.ForegroundColor = ConsoleColor.Blue;
+                else Console.BackgroundColor = ConsoleColor.Blue;
+            }
+            break;
+        case "White":
+            {
+                if (F_or_B) Console.ForegroundColor = ConsoleColor.White;
+                else Console.BackgroundColor = ConsoleColor.White;
+            }
+            break;
+    }
+}
+class MyEventsArgs : EventArgs
+{
+    public char ch;
+}
+class KeyEvent
+{
+    public event EventHandler<MyEventsArgs>? KeyDown;
+    public void OnKeyDown(char ch)
+    {
+        MyEventsArgs c= new MyEventsArgs();
+        if(KeyDown != null)
+        {
+            c.ch = ch;
+            KeyDown(this, c);
+        }
+    }
+}
--- Lesson15/Program.cs
+++ Lesson15/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿MyEvent myEvent = new MyEvent();
+UserInfo user = new UserInfo("Alex", "Hilton", 45);
+myEvent.UserEvent += user.UserInfoHandler;
+myEvent.OnUserEvent();
+
+class UserInfo
+{
+    public string? Name { get; set; }
+    public string? Family { get; set; }
+    public decimal? Salary { get; set; }
+    public UserInfo(string? name, string? family, decimal? salary)
+    {
+        Name = name;
+        Family = family;
+        Salary = salary;
+    }
+    public override string? ToString()
+    {
+        return Name + " " + Family + " " + Salary;
+    }
+    public void UserInfoHandler()
+    {
+        Console.WriteLine("Событие вызвано!");
+        Console.WriteLine($"Имя:{Name} Фамилия:{Family} Зарплата:{Salary}");
+    }
+}
+class MyEvent
+{
+    UI[] events=new UI[5];
+    public event UI UserEvent
+    {
+        add => events[1] = value;
+        remove=> events[1] = null!;
+    }
+    public void OnUserEvent() => events[1]();
+}
+delegate void UI();
