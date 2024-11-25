b780199cdcc966a109c3ddea67ff8a3e0db1d200
rakdav <rakdav@inbox.ru>
2024-11-25 20:16:00 +0200

    Lesson22.1

---
+++ Lesson22.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using OfficeOpenXml;
+using System.ComponentModel;
+
+Tovar[] mas =
+{
+    new Tovar("Обои виниловые",TypeOfTovar.Строительство,
+    150,3),
+    new Tovar("Обои флизелиновые",TypeOfTovar.Строительство,
+    250,5),
+    new Tovar("Обои бумажные",TypeOfTovar.Строительство,
+    100,2),
+    new Tovar("Обои жидкие",TypeOfTovar.Строительство,
+    90,10),
+    new Tovar("Штукатурка “Соседи не услышат",TypeOfTovar.Строительство,
+    120,15),
+    new Tovar("Шпатлевка \"Сладко да гладко\"",TypeOfTovar.Строительство,
+    34,12),
+    new Tovar("Дрель \"Буровик 3000\"",TypeOfTovar.Электроинструмент,
+    250,5),
+    new Tovar("Молоток \"Тор\"",TypeOfTovar.Электроинструмент,
+    250,5),
+    new Tovar("Сок апельсиновый",TypeOfTovar.Напитки,
+    23,500),
+    new Tovar("Чипсы со вкусом зелени",TypeOfTovar.Закуска,
+    2,245)
+};
+FileInfo fileInfo = new FileInfo("МагазинМечты.xlsx");
+if(!fileInfo.Exists)fileInfo.Create();
+ExcelPackage.LicenseContext =
+    OfficeOpenXml.LicenseContext.NonCommercial;
+ExcelPackage newBook = new ExcelPackage("МагазинМечты.xlsx");
+newBook.Workbook.Worksheets.Add("Складской учет");
+ExcelWorksheet worksheet =
+    newBook.Workbook.Worksheets["Складской учет"];
+worksheet.Cells["A1"].Value = "Наименование";
+worksheet.Cells["B1"].Value = "Тип товара";
+worksheet.Cells["C1"].Value = "Стоимость";
+worksheet.Cells["D1"].Value = "Количество";
+for (int i = 0; i < mas.Length; i++)
+{
+    worksheet.Cells[2 + i, 1].Value = mas[i].Name;
+    worksheet.Cells[2 + i, 2].Value = mas[i].TypeOfTovar;
+    worksheet.Cells[2 + i, 3].Value = mas[i].Price;
+    worksheet.Cells[2 + i, 4].Value = mas[i].Count;
+}
+newBook.Save();
+newBook.SaveAs(fileInfo);
+class Tovar
+{
+    public string? Name { get; set; }
+    public TypeOfTovar TypeOfTovar { get; set; }
+    public decimal Price {  get; set; } 
+    public int Count {  get; set; }
+
+    public Tovar(string? name, TypeOfTovar typeOfTovar,
+        decimal price, int count)
+    {
+        Name = name;
+        TypeOfTovar = typeOfTovar;
+        Price = price;
+        Count = count;
+    }
+}
+enum TypeOfTovar
+{
+    Канцелярия,
+    Молочная,
+    Закуска,
+    Электроинструмент,
+    Напитки,
+    Строительство
+}
---
+++ Lesson22/Program.cs
@@ -0,0 +1,63 @@
+﻿using OfficeOpenXml;
+string filePath = "ListExcel.xlsx";
+ExcelPackage.LicenseContext =
+    LicenseContext.NonCommercial;
+ExcelPackage newBook = new ExcelPackage(filePath);
+ExcelWorksheet worksheet1 = 
+    newBook.Workbook.Worksheets["Лист1"];
+//newBook.Workbook.Worksheets.Add("Лист2");
+//newBook.Save();
+//ExcelWorksheet worksheet2 = 
+//    newBook.Workbook.Worksheets["Лист2"];
+//if(worksheet2!= null)
+//{
+//    newBook.Workbook.Worksheets.Delete(worksheet2);
+//    newBook.Save();
+//}
+//worksheet1.Cells["A1"].Value = "FIO";
+//worksheet1.Cells["B1"].Value = "Age";
+//worksheet1.Cells["C1"].Value = "Weight";
+//newBook.Save();
+//List<Person> list = new List<Person>()
+//{
+//    new Person(){FIO="Иван Иванов", Age=23,Weight=78},
+//    new Person(){FIO="Сергей Михеев", Age=19,Weight=67},
+//    new Person(){FIO="Алексей Алексеев", Age=34,Weight=80},
+//    new Person(){FIO="Михаил Семенов", Age=28,Weight=74}
+//};
+//int k = 2;
+//for (int i = 0; i < list.Count; i++)
+//{
+//    worksheet1.Cells[k+i, 1].Value = list[i].FIO;
+//    worksheet1.Cells[k+i, 2].Value = list[i].Age;
+//    worksheet1.Cells[k+i, 3].Value = list[i].Weight;
+//}
+//newBook.Save();
+Console.Write("Введите строку для поиска:");
+string str=Console.ReadLine()!;
+int row,column;
+foreach(var cell in worksheet1.Cells)
+{
+    if (cell.Text == str)
+    {
+        row = cell.Start.Row; 
+        column = cell.Start.Column;
+        Console.WriteLine($"Значение {str}" +
+            $" найдено в ячейке с " +
+            $"координатами { row }:{ column }");
+    }
+}
+class Person
+{
+    public string? FIO { get; set; }
+    public int? Age { get; set; }
+    public double? Weight { get; set; }
+}
+
+
+
+
+
+
+
+
