e26ddcb495855ae026778dc7b695317ccf8c7243
rakdav <rakdav@inbox.ru>
2024-12-09 20:24:12 +0200

    Les26-1

---
+++ Lesson26/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson26
+{
+
+    [Serializable]
+    public class Product
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int Period { get; set; }
+        public string? Manufactorer { get; set; }
+        public string? Category { get; set; }
+    }
+}
---
+++ Lesson26/Program.cs
@@ -0,0 +1,36 @@
+﻿using Lesson26;
+using System.Xml.Serialization;
+Product product1 = new Product()
+{
+    Name = "Хлеб",
+    Category="Белый",
+    Period=8,
+    Manufactorer="Хлебпром",
+    Price=40
+};
+Product product2 = new Product()
+{
+    Name="Молоко",
+    Category="15%",
+    Period=5,
+    Manufactorer="Молокозавод",
+    Price=39
+};
+Product product3 = new Product()
+{
+    Name = "Сметана",
+    Category = "20%",
+    Period = 10,
+    Manufactorer = "Молокозавод",
+    Price = 75
+};
+List<Product> products = new List<Product>();
+products.Add(product1);
+products.Add(product2);
+products.Add(product3);
+XmlSerializer xmlSerializer = 
+    new XmlSerializer(typeof(List<Product>));
+FileStream stream = new FileStream("products.xml",
+    FileMode.Create);
+xmlSerializer.Serialize(stream, products);
+stream.Close();
