e67eab171c184ae2847ce5a1c55cf4d9df56027b
rakdav <rakdav@inbox.ru>
2025-02-21 21:21:00 +0200

    Les46

---
+++ Les46.1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Les46._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            //myRectangle.Style.Setters.Clear();
+            Style existingStyle = (Style)this.Resources["rectangleStyle"];
+            Style newStyle = new Style(typeof(System.Windows.Shapes.Rectangle),existingStyle);
+            Setter[] styleSetters = new Setter[]
+            {
+                new Setter(Shape.FillProperty, new SolidColorBrush(Colors.Blue)),
+                new Setter(WidthProperty, 250.0),
+                new Setter(HeightProperty, 200.0),
+            };
+            foreach (Setter setter in styleSetters)
+            {
+                newStyle.Setters.Add(setter);
+            }
+            myRectangle.Style = newStyle;
+        }
+    }
+}
---
+++ Lesson46/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson46
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isGradient;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isGradient = !isGradient)
+            {
+                var gradientBrush = new RadialGradientBrush(Colors.Black, Colors.Blue);
+                Resources["EllipseGradient"] = gradientBrush;
+            }
+            else
+            {
+                GradientStopCollection gradientStops = new GradientStopCollection();
+                gradientStops.Add(new GradientStop(Colors.Yellow, 0.1));
+                gradientStops.Add(new GradientStop(Colors.Red, 0.5));
+                gradientStops.Add(new GradientStop(Colors.Green, 1.0));
+                var gradientBrush = new RadialGradientBrush(gradientStops);
+                Resources["EllipseGradient"] = gradientBrush;
+            }
+        }
+    }
+}
