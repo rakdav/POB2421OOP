6acad1e3cdc7f88bf3b9916e3fab9ea47a1e49bc
rakdav <rakdav@inbox.ru>
2025-02-24 20:43:50 +0200

    Lse47.2

--- les47.1/MainWindow.xaml.cs
+++ les47.1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace les47._1
 {
@@ -16,11 +19,22 @@ namespace les47._1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Person> people = new();
         public Person Person { get; set; }
+        
+    private int next = 0;
         public MainWindow()
         {
-            Person = new Person() { Name = "Вася", Surname = "Пупкин", Age = 20, BirthDate = DateTime.Now.AddYears(-20).ToString() };
+            people.Add(new Person() { Name = "Вася", Surname = "Пупкин", Age = 20, BirthDate = DateTime.Now.AddYears(-20).ToString()});
+            people.Add(new Person() { Name = "Боря", Surname = "Васильев", Age = 25, BirthDate = DateTime.Now.AddYears(-25).ToString() });
+            people.Add(new Person() { Name = "Иван", Surname = "Соловьев", Age = 18, BirthDate = DateTime.Now.AddYears(-18).ToString() });
             InitializeComponent();
+            Person = people[next];
+            DataContext = Person;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Person = people[++next];
             DataContext = Person;
         }
     }
--- les47.1/Person.cs
+++ les47.1/Person.cs
@@ -1,16 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace les47._1
 {
-    public class Person
+    public class Person:INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int Age { get; set; }
-        public string BirthDate { get; set; }
+        private string name;
+        public string Name
+        {
+            get=>name;
+            set
+            {
+                name=value;
+                OnPropertyChanged("Name");
+            }
+        }
+        private string surname;
+        public string Surname
+        {
+            get => surname;
+            set
+            {
+                surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get=> age;
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+        private string birthDate;
+        public string BirthDate
+        {
+            get => birthDate;
+            set
+            {
+                birthDate = value;
+                OnPropertyChanged("BirthDate");
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
