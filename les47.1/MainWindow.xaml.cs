0c90e541828ef22370a246df7461e7c528c183d4
rakdav <rakdav@inbox.ru>
2025-02-24 19:57:23 +0200

    les46.1

---
+++ les47.1/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace les47._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Person Person { get; set; }
+        public MainWindow()
+        {
+            Person = new Person() { Name = "Вася", Surname = "Пупкин", Age = 20, BirthDate = DateTime.Now.AddYears(-20).ToString() };
+            InitializeComponent();
+            DataContext = Person;
+        }
+    }
+}
---
+++ les47.1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les47._1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
