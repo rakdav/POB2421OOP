bcd78375e3c4e0861c10faaa5b86d6cd415221dc
rakdav <rakdav@inbox.ru>
2025-02-24 21:20:27 +0200

    Lesson47.3

---
+++ Lesson47.3/ApplicationViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson47._3
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone? selectedPhone;
+        public Phone? SelectedPhone
+        {
+            get => selectedPhone;
+            set
+            {
+                selectedPhone = value;
+                OnProperyChanged(nameof(SelectedPhone));
+            }
+        }
+        public ObservableCollection<Phone> Phones { get; set; }
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone{Title="Samsung A55",Company="Samsung",Price=40000},
+                new Phone{Title="Galaxy s10",Company="Samsung",Price=90000},
+                new Phone{Title="IPhone 14",Company="Apple",Price=75000},
+                new Phone{Title="IPhone 15",Company="Apple",Price=120000}
+            };
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnProperyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ Lesson47.3/Phone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson47._3
+{
+    public class Phone : INotifyPropertyChanged
+    {
+        private string title;
+        private string company;
+        private int price;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title= value;
+                OnProperyChanged(nameof(Title));
+            }
+        }
+        public string Company
+        {
+            get => company;
+            set
+            {
+                company = value;
+                OnProperyChanged(nameof(Company));
+            }
+        }
+        public int Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                OnProperyChanged(nameof(Price));
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnProperyChanged([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
